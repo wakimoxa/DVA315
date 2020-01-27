a34a2711af358b62a0b4c7bafb9287452c491bd2
UnknownPlayer99 <filip.starkenberg@gmail.com>
2020-01-27 19:16:29 +0100

    updated wrapper

--- labb1/SOURCE_FILES/wrapper.c
+++ labb1/SOURCE_FILES/wrapper.c
@@ -9,25 +9,36 @@ int MQcreate (mqd_t * mq, char * name)
 {
  	//Should create a new messagequeue, use mq as reference pointer so you can reach the handle from anywhere
 	//Should return 1 on success and 0 on fail
+    *mq = mq_open(name, O_RDWR);
+    if(*mq == NULL){
+        return 0;
+    }
+    return 1;
 
 }
 int MQconnect (mqd_t * mq, char * name)
 {
-    /* Connects to an existing mailslot for writing Uses mq as reference pointer, so that you can 	reach the handle from anywhere*/
+    /* Connects to an existing mailslot for writing Uses mq as reference pointer, so that you can reach the handle from anywhere*/
     /* Should return 1 on success and 0 on fail*/
+    
+
 }
 
 void * MQread (mqd_t mq, void ** buffer)
 {
 
     /* Read a msg from a mailslot, return nr Uses mq as reference pointer, so that you can 		reach the handle from anywhere */
-    /* should return number of bytes read              */
+    /* should return number of bytes read*/
+    return mq_receive(mq, *buffer , MAX_SIZE, NULL);
 }
 
 int MQwrite (mqd_t mq, void * sendBuffer)
 {
     /* Write a msg to a mailslot, return nr Uses mq as reference pointer, so that you can 	     reach the handle from anywhere*/
     /* should return number of bytes read         */
+    if(mq_send(mq, sendBuffer, sizeof(sendBuffer), NULL) == 0)
+        return 1;
+    return 0;
 }
 
 int MQclose(mqd_t * mq, char * name)
@@ -35,6 +46,7 @@ int MQclose(mqd_t * mq, char * name)
     /* close a mailslot, returning whatever the service call returns Uses mq as reference pointer, so that you can
     reach the handle from anywhere*/
     /* Should return 1 on success and 0 on fail*/
+    return mq_close(mq);
 }
 
 
